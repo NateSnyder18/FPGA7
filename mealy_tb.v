b425dbc86a4d4435e81d3962d5c8ebb6916e69e8
NateSnyder18 <123413999+NateSnyder18@users.noreply.github.com>
2024-11-03 17:31:54 -0500

    Create mealy_tb.v

---
+++ mealy_tb.v
@@ -0,0 +1,22 @@
+module mealy_tb();
+    reg x, clk, rst;
+    wire z;
+   
+    mealy uut(x, z, clk, rst);
+   
+    always begin
+        #10 clk = ~clk;
+    end
+   
+    initial begin
+        x = 0; rst = 0; clk = 0;
+        #5 x = 1;
+        #40 x = 0;
+        #20 x = 1;
+        #20 rst = 1;
+        #20 rst = 0; x = 1;
+        #20 x = 0;
+        #20 x = 1;
+        #20 $stop;
+    end
+endmodule
